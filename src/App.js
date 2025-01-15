9577cd651d870769548000a5a6b78e1daa5abfda
Tom Necke <tomnecke.pro@gmail.com>
2025-01-15 17:00:55 +0100

    add simple form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
 
+let resultFee = 0;
+let resultTotal = 0;
+
+
+const handleSubmit = (event) => {
+  alert("test")
+  event.preventDefault()
+  resultFee = 12
+  console.log(resultFee)
+}
+
 function App() {
   return (
     <div className="App">
       <h1>PayPal Gebührenrechner</h1>
+      <form onSubmit={handleSubmit}>
+        <label for="inputNum">Betrag:</label>
+        <input id='inputNum' type='number' />
+        <fieldset>
+          <legend>Type</legend>
+          <div>
+            <label for="feeRadio">Gebühren berechnen</label>
+            <input type='radio' id='feeRadio' value="feeRadio" name='selectCalc' />
+          </div>
+          <div>
+            <label for="totalRadio">Endsumme berechnen</label>
+            <input type='radio' id='totalRadio' value="totalRadio" name='selectCalc'/>
+          </div>
+        </fieldset>
+        <input type='submit' value="berechnen" />
+      </form>
+      <div>
+        <h2>Ergebnis</h2>
+        <p>PayPal Gebühren: {resultFee}</p>
+        <p>Betrag: {resultTotal}</p>
+      </div>
     </div>
   );
 }
