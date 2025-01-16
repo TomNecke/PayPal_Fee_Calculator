08c743b965ac9870c83e17d6e72192221b613c0c
Tom Necke <tomnecke.pro@gmail.com>
2025-01-16 13:16:31 +0100

    basic maths

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,21 +9,26 @@ export default function () {
     const [type, setType] = useState("sendRadio");
     const [feeRate, setFeeRate] = useState("0");
 
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
     const fee = [{ Gebühr: 2.49, Festgebühr: 0.35 }, { Gebühr: 0.00, Festgebühr: 0.00 }, { Gebühr: 1.50, Festgebühr: 0.35 }, { Gebühr: 10.00, Festgebühr: 0.10 }, { Gebühr: 0.90, Festgebühr: 0.00 }, { Gebühr: 2.49, Festgebühr: 0.35 }, { Gebühr: 2.19, Festgebühr: 0.35 }, { Gebühr: 1.99, Festgebühr: 0.35 }, { Gebühr: 1.49, Festgebühr: 0.35 }]
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
+    const handleSubmit = async (event) => {
+        await delay(1000);
         let input = Number.parseInt(inputNum)
 
-        setResultSend(input - (input / 100 * fee[feeRate].Gebühr + fee[feeRate].Festgebühr))
-        setResultReceive()
+        let sendOutput = input - (input / 100 * fee[feeRate].Gebühr + fee[feeRate].Festgebühr)
+        let receiveOutput = (input / (1 - (fee[feeRate].Gebühr / 100))) + 0.35
+
+        setResultSend(sendOutput.toFixed(2))
+        setResultReceive(receiveOutput.toFixed(2))
 
     }
 
     return (
         <div>
             <h1>PayPal Gebührenrechner</h1>
-            <form onSubmit={handleSubmit}>
+            <form onChange={handleSubmit}>
                 <fieldset onChange={(e) => { setType(e.target.value) }} className='radioSendReceive'>
                     <div>
                         <label htmlFor="sendRadio">Geld senden</label>
@@ -50,16 +55,12 @@ export default function () {
                     <option value="7">{"Händlerkonditionen 5.000 - 25.000 €"}</option>
                     <option value="8">{"Händlerkonditionen > 25.000 €"}</option>
                 </select>
-                <input type='submit' value="Berechnen" />
             </form>
             <div>
                 <h2>Ergebnis</h2>
                 {type == "sendRadio" ? <p>Betrag senden: {resultSend}</p> : <p>Betrag empfangen: {resultReceive}</p>}
                 <p>Gebührenrate: {fee[feeRate].Gebühr}</p>
                 <p>Festgebühr: {fee[feeRate].Festgebühr}</p>
-                <h3>devInfo</h3>
-                <p>inputNum: {inputNum}</p>
-                <p>type: {type}</p>
             </div>
         </div>
     )
