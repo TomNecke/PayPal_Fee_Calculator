966df15899f892dade3513a5208b1a233a81d6a9
Tom Necke <tomnecke.pro@gmail.com>
2025-01-16 09:58:18 +0100

    add select

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,8 +4,8 @@ import React, { useState } from 'react'
 export default function () {
 
     const [inputNum, setInputNum] = useState(null);
-    const [resultFee, setResultFee] = useState(null);
-    const [resultTotal, setResultTotal] = useState(null);
+    const [resultSend, setResultSend] = useState(null);
+    const [resultReceive, setResultReceive] = useState(null);
 
     const handleSubmit = (event) => {
         event.preventDefault()
@@ -13,8 +13,8 @@ export default function () {
         let result = Number.parseInt(inputNum)
         console.log(result+(result/100*2.49)+0.35);
         
-        console.log("resultFee: " + resultFee)
-        console.log("resultTotal: " + resultTotal)
+        console.log("resultSend: " + resultSend)
+        console.log("resultReceive: " + resultReceive)
         console.log("inputNum: " + inputNum)
     }
 
@@ -29,20 +29,33 @@ export default function () {
                 <fieldset>
                     <legend>Type</legend>
                     <div>
-                        <label htmlFor="feeRadio">Gebühren berechnen</label>
-                        <input type='radio' id='feeRadio' value="feeRadio" name='selectCalc' />
+                        <label htmlFor="sendRadio">Geld senden</label>
+                        <input type='radio' id='sendRadio' value="sendRadio" name='selectCalc' />
                     </div>
                     <div>
-                        <label htmlFor="totalRadio">Endsumme berechnen</label>
-                        <input type='radio' id='totalRadio' value="totalRadio" name='selectCalc' />
+                        <label htmlFor="receiveRadio">Geld empfangen</label>
+                        <input type='radio' id='receiveRadio' value="receiveRadio" name='selectCalc' />
                     </div>
                 </fieldset>
-                <input type='submit' value="berechnen" />
+                <label htmlFor='selectFeeRate'>Art der Zahlung</label>
+                <select name='feeRate' id='selectFeeRate'>
+                    <option value="1">Waren oder Dienstleistungen bezahlen</option>
+                    <option value="2">Zahlung an Freunde und Familie</option>
+                    <option value="3">Spenden sammeln</option>
+                    <option value="4">Mikrozahlung</option>
+                    <option value="5">Zahlung mit QR-Code</option>
+                    <option value="6">{"Händlerkonditionen < 2.000 €"}</option>
+                    <option value="7">{"Händlerkonditionen 2.000 - 5.000 €"}</option>
+                    <option value="8">{"Händlerkonditionen 5.000 - 25.000 €"}</option>
+                    <option value="9">{"Händlerkonditionen > 25.000 €"}</option>
+                </select>
+                <input type='submit' value="Berechnen" />
             </form>
             <div>
                 <h2>Ergebnis</h2>
-                <p>PayPal Gebühren: {resultFee}</p>
-                <p>Betrag: {resultTotal}</p>
+                <p>inputNum: {inputNum}</p>
+                <p>Betrag senden: {resultSend}</p>
+                <p>Betrag empfangen: {resultReceive}</p>
             </div>
         </div>
     )
